05b0626e7ffb6b55efae16e226bf5a3f40415507
Tochukwu <tochukwusage4@gmail.com>
2024-05-15 20:03:43 +0100

    Added enums and the db entity class

---
+++ CapitalProject.Data/Entities/CustomQuestion.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalProject.Data.Entities
+{
+    public class CustomQuestion
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public QuestionType QuestionType { get; set; }
+        public string? Question { get; set; }
+        public string? ParagraphAnswer { get; set; }
+        public bool? YesNoAnswer { get; set; }
+        public string? DropDownAnswer { get; set; }
+        public string? MultipleChoiceAnswer { get; set; }
+        public DateTime? Date { get; set; }
+        public int? NumericalAnswer { get; set; }
+        [JsonProperty(PropertyName = "maxChoiceAllowed")]
+        public int MaxChoiceAllowed { get; set; }
+    }
+}
---
+++ CapitalProject.Data/Enums/Enumerations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalProject.Data.Enums
+{
+    public class Enumerations
+    {
+        public enum QuestionType
+        {
+            Paragraph = 1,
+            YesorNo,
+            Dropdown,
+            MultipleChoice,
+            Date,
+            Number,
+        }
+    }
+}
