d26faad413f88424198366b42d4606992cdfbb01
Tochukwu <tochukwusage4@gmail.com>
2024-05-15 20:01:51 +0100

    Added the needed dtos

---
+++ CapitalProject.Data/DTOs/CreateCustomQuestionDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalProject.Data.DTOs
+{
+    public class CreateCustomQuestionDTO
+    {
+        public QuestionType QuestionType { get; set; }
+        public string? Question { get; set; }
+        public string? ParagraphAnswer { get; set; }
+        public bool? YesNoAnswer { get; set; }
+        public string? DropDownAnswer { get; set; }
+        public string? MultipleChoiceAnswer { get; set; }
+        public DateTime? Date { get; set; }
+        public int? NumericalAnswer { get; set; }
+        public int MaxChoiceAllowed { get; set; }
+    }
+    public class UpdateCustomQuestionDTO
+    {
+        public QuestionType QuestionType { get; set; }
+        public string? Question { get; set; }
+    }
+
+    public class AnswerQuestionDTO
+    {
+        public int Answer { get; set; }
+        public string StringAnswer { get; set; }
+    }
+}
---
+++ CapitalProject.Data/DTOs/DisplayCustomQuestionDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalProject.Data.DTOs
+{
+    public class DisplayCustomQuestionDTO
+    {
+        public QuestionType QuestionType { get; set; }
+        public string? Question { get; set; }
+        public string? ParagraphAnswer { get; set; }
+        public bool? YesNoAnswer { get; set; }
+        public string? DropDownAnswer { get; set; }
+        public string? MultipleChoiceAnswer { get; set; }
+        public DateTime? Date { get; set; }
+        public int? NumericalAnswer { get; set; }
+        public int MaxChoiceAllowed { get; set; }
+    }
+    public class DisplayCustomQuestionsCandidate
+    {
+        public QuestionType QuestionType { get; set; }
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
+    }
+}
